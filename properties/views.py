e863953e4c9a0482a35740e984bb3077d312b0b9
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-07-13 22:27:00 +0100

    Implement property listing feature with caching; add URL routing and HTML template

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+
+@cache_page(60 * 15)  # Cache for 15 minutes (60 seconds * 15)
+def property_list(request):
+    properties = Property.objects.all()
+    return render(request, "properties/list.html", {"properties": properties})
